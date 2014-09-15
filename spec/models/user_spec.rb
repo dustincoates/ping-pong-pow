aa83a261be96fac79fd4a6e3dfce68d927cbfc55
xanderwebs <xanderwebs@gmail.com>
2014-09-14 21:37:42 -0400

    Add tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  describe 'validations' do
+    let(:valid_params) do
+      { 
+        name: 'Peter Thiel', 
+        email: 'pt@foundersfund.com' 
+      }
+    end
+
+    it 'is valid with valid params' do
+      expect(User.new(valid_params)).to be_valid
+    end
+
+    it 'requires a name' do
+      expect(User.new(valid_params.except(:name))).to_not be_valid
+    end
+
+    it 'requires an email' do
+      expect(User.new(valid_params.except(:email))).to_not be_valid
+    end
+
+    it 'requires a unique email' do
+      User.create(valid_params)
+      user = User.new(name: 'Same email', email: valid_params[:email])
+      expect(user).to_not be_valid
+    end
+  end
 end
